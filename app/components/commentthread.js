29dd0640b6fa8402ee8956dd3ea3b31d2a78d40e
Minxin Gao <mgao@umass.edu>
2016-11-07 00:23:54 -0500

    fb6

--- app/components/commentthread.js
+++ app/components/commentthread.js
@@ -4,18 +4,18 @@ import CommentEntry from './commententry';
 export default class CommentThread extends React.Component {
   render() {
     return (
-     <ul className="media-list">
-       {React.Children.map(this.props.children, function(child) {
-         return (
-           <li className="media">
-             {child}
-           </li>
-         )
-       })}
-       <li className="media">
-         <CommentEntry />
-       </li>
-     </ul>
-   )
- }
+      <ul className="media-list">
+        {React.Children.map(this.props.children, function(child) {
+          return (
+            <li className="media">
+              {child}
+            </li>
+          )
+        })}
+        <li className="media">
+          <CommentEntry onPost={this.props.onPost} />
+        </li>
+      </ul>
+    )
+  }
 }
